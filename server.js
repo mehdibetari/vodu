17eaf9e2b0aafc05478a87eb1831dd21af388969
mehdibetari <mehdi.betari@gmail.com>
2017-09-19 23:57:42 +0200

    feat(front): Use list template with data

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ app.get('/', function(req, res) {
     res.redirect('/calendrier/netflix');
 });
 app.get('/calendrier/netflix', function(req, res){
-    fs.readFile('./front-layout/test.html', 'utf8', function (err,data) {
+    fs.readFile('./front-layout/agenda-netflix.html', 'utf8', function (err,data) {
         if (err) {
-            return;
+            return console.log(err);
         }
         fs.readFile('./output/netflix-upcoming.json', 'utf8', function (error,response) {
             if (error) {
-                return;
+                return console.log(error);
             }
             var tmpl = dust.compile(data, 'view-netflix');
             dust.loadSource(tmpl);
-            var view = dust.render('view-netflix', { emoj: response }, function(err, out) {
-                if(err) {
-                    console.error(err);
+            var view = dust.render('view-netflix', { list: JSON.parse(response) }, function(e, out) {
+                if(e) {
+                    console.error(e);
                 } else {
                     // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
                     res.send(out);
