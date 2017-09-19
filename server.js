83f556131599298dd3a6a424fa8b69e03c6056da
mehdibetari <mehdi.betari@gmail.com>
2017-09-19 22:59:20 +0200

    feat(server): Wrap response json

--- server.js
+++ server.js
@@ -8,23 +8,25 @@ app.get('/', function(req, res) {
 app.get('/calendrier/netflix', function(req, res){
     fs.readFile('./front-layout/test.html', 'utf8', function (err,data) {
         if (err) {
-            return console.log(err);
+            return;
         }
-        console.log(data);
-        var tmpl = dust.compile(data, 'view-netflix');
-        dust.loadSource(tmpl);
-        var view = dust.render('view-netflix', { emoj: ';)' }, function(err, out) {
-            if(err) {
-                console.error(err);
-            } else {
-                // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-                res.send(out);
+        fs.readFile('./output/netflix-upcoming.json', 'utf8', function (error,response) {
+            if (error) {
+                return;
             }
+            var tmpl = dust.compile(data, 'view-netflix');
+            dust.loadSource(tmpl);
+            var view = dust.render('view-netflix', { emoj: response }, function(err, out) {
+                if(err) {
+                    console.error(err);
+                } else {
+                    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+                    res.send(out);
+                }
+            });
         });
-
     });
 });
 
 app.listen('80');
-console.log('Magic happens on port 8081');
 exports = module.exports = app;
