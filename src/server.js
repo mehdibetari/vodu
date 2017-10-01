678171b80e24883eaf689366640c1bbded14edf0
mehdibetari <mehdi.betari@gmail.com>
2017-10-02 00:30:46 +0200

    Fix(sitemap): Dirname

--- src/server.js
+++ src/server.js
@@ -30,6 +30,6 @@ app.get('/calendrier/netflix', function(req, res){
     });
 });
 
-app.use(express.static('sitemap'));
+app.use(express.static(__dirname + '/sitemap'));
 app.listen('80');
 exports = module.exports = app;
