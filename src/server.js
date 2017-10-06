08f530a32794492d08e4ef8f81abc62499c1ffdd
mehdibetari <mehdi.betari@gmail.com>
2017-10-07 01:25:16 +0200

    Add every week netflix calendar

--- src/server.js
+++ src/server.js
@@ -32,6 +32,34 @@ app.get('/calendrier/netflix', function(req, res){
     });
 });
 
+app.get('/episode-chaque-semaine/netflix', function(req, res){
+    fs.readFile('./front-layout/every-week-netflix.html', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        fs.readFile('./public/netflix-every-week.json', 'utf8', function (error,response) {
+            if (error) {
+                return console.log(error);
+            }
+            var tmpl = dust.compile(data, 'view-netflix');
+            dust.loadSource(tmpl);
+            const netflixEveryWeekData = JSON.parse(response);
+            const lastUpdateDate = new Date(netflixEveryWeekData.timeStamp);
+            const fullDate = lastUpdateDate.getDate()+'.'+(lastUpdateDate.getMonth()+1)+'.'+lastUpdateDate.getFullYear();
+            const fullTime = lastUpdateDate.getHours()+'h'+(lastUpdateDate.getMinutes()<10?'0':'') + lastUpdateDate.getMinutes();
+            const fullDateTime = fullDate+' à '+fullTime;
+            var view = dust.render('view-netflix', { list: netflixEveryWeekData.items, lastUpdate: fullDateTime, theFooter: netflixEveryWeekData.footer}, function(e, out) {
+                if(e) {
+                    console.error(e);
+                } else {
+                    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+                    res.send(out);
+                }
+            });
+        });
+    });
+});
+
 app.use(express.static('./public'));
 app.listen('80');
 exports = module.exports = app;
