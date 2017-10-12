01706ff2f401a17c63fdee4dabb9e9da758ff150
mehdibetari <mehdi.betari@gmail.com>
2017-10-12 08:29:23 +0200

    WIP

---
+++ src/media-store/Media.js
@@ -0,0 +1,45 @@
+'use strict';
+
+class Media {
+
+    constructor(media) {
+        this.oid = media.id;
+        this.name = media.name;
+        this.type = media.type;
+        this.premiereDate = media.premiereDate;
+        this.seasons = media.seasons;
+        this.ourl = media.uri;
+        this.actors = media.actors;
+        this.directors = media.directors;
+        this.creators = media.creators;
+        this.posterUrl = media.posterUrl;
+        this.summary = media.summary;
+        this.imdbPath = media.mediaLink;
+    }
+
+}
+module.exports =  Media;
+
+"id": 4846,
+"categoryId": "titles",
+"category": "Only On Netflix",
+"name": "Club De Cuervos",
+"description": "",
+"runTime": "",
+"type": "series",
+"distribution": "global",
+"distributionComments": "",
+"specialFormats": "",
+"premiereDate": "29 septembre 2017",
+"seasons": 2,
+"uri": "/fr/only-on-netflix/4846",
+"locale": "fr",
+"sortDate": "2017-09-29",
+"actors": "Luis Gerardo Méndez, Mariana Treviño, Stephanie Cayo...",
+"directors": "",
+"creators": "Gary Alazraki, Michael Lam...",
+"posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk1NDQ3NzY1Ml5BMl5BanBnXkFtZTgwMzM4ODc0MzI@._V1_UY268_CR1,0,182,268_AL_.jpg",
+"mediaLink": "/title/tt4680240/?ref_=fn_al_tt_1",
+"summary": "\n                    When the patriarch of a prominent family dies, his heirs battle to determine who will gain control of his beloved soccer team: The Cuervos of Nuevo Toledo.\n            ",
+"localPath": "https://ucarecdn.com/080267c0-08c7-4368-9510-653491e07033/MV5BMTk1NDQ3NzY1Ml5BMl5BanBnXkFtZTgwMzM4ODc0MzI_V1_UY268_CR10182268_AL_.jpg",
+"uuid": "9ca8db10-aecd-11e7-8a70-fd15344748aa"
