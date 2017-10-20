796c10da7a7fb2e20dadb2d2007cb756c1fb296e
mehdibetari <mehdi.betari@gmail.com>
2017-10-21 01:45:35 +0200

    feat(scraper): Use current year to found media title & use filestore to download poster

--- src/scrapers/netflix-scraper.js
+++ src/scrapers/netflix-scraper.js
@@ -1,29 +1,11 @@
-let fs = require('fs');
-let request = require('request');
-let cheerio = require('cheerio');
-let colors = require('colors');
-let uploadcare = require('uploadcare')('677798e572bfce424b48', '159a48768c425d3918b5');
-
+let request       = require('request');
+let cheerio       = require('cheerio');
+let colors        = require('colors');
+const Filestorage = require('../media-storage/Filestorage');
 
 const NETFLIX_BASE_URL = 'https://media.netflix.com';
 
-function download (uri, filename, callback){
-    if (!uri) return callback(false);
-    request.head(uri, function(err, res, body){
-        // console.log('content-type:', res.headers['content-type']);
-        // console.log('content-length:', res.headers['content-length']);
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', function(){
-            //Upload from URL
-            uploadcare.file.fromUrl(uri, function(err,res){
-                //Res should contain returned file ID
-                let uplc_filepath = 'https://ucarecdn.com/' + res.file_id + '/' + res.filename;
-                callback(uplc_filepath);
-            });
-        });
-    });
-}
-
-function getPoster (uri, name, year, callback) {
+function getPoster (uri, name, year, uploadcare, callback) {
     console.log(colors.inverse('NETFLIX poster to found'),colors.bgGreen.white(name,year));
     request(NETFLIX_BASE_URL+uri, function(error, response, html){
         if(!error){
@@ -35,7 +17,8 @@ function getPoster (uri, name, year, callback) {
             if (posterUri && posterUri !== '' && posterUri.length > 0) {
                 posterUri = $(posterUri).attr('src');
                 console.log('    NETFLIX POSTER URL : '+posterUri);
-                download(posterUri,'./public/posters/' + name + '+' + year + '.jpg', function (path) {
+                let posterStore = new Filestorage();
+                posterStore.download(posterUri,'./public/posters/' + name + '+' + year + '.jpg', uploadcare, function (path) {
                     const addedMessage = '    ✓ Poster '+ colors.green('ADDED') + ' at ' + path;
                     const failedMessage = colors.bgRed.white('POSTER SEARCH ABORDED') + ' => ' + name + year + colors.magenta(' ✘ Poster DOES NOT downloaded');
                     console.log( (path) ? addedMessage : failedMessage);
