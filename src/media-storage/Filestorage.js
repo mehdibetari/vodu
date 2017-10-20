a270b2d552e7f26362891ba88ffd52fae1ecff22
mehdibetari <mehdi.betari@gmail.com>
2017-10-21 01:46:10 +0200

    feat(Filestore): Finish

--- src/media-storage/Filestorage.js
+++ src/media-storage/Filestorage.js
@@ -1,3 +1,8 @@
+const configKeys = require('../config-keys');
+let fs           = require('fs');
+let request      = require('request');
+let uploadcare   = require('uploadcare')(configKeys.uploadcare.public_key, configKeys.uploadcare.private_key);
+
 const STORE_UPLOADCARE_BASE_URL = 'https://ucarecdn.com/';
 
 class Filestorage {
@@ -6,16 +11,16 @@ class Filestorage {
 
     }
 
-    download (uri, filename, callback, local, distant) {
+    download (uri, filename, uploadcare, callback) {
         if (!uri) return callback(false);
-        if (local) {
-            downloadLocaly(uri, filename, function (state) {
-                callback(state);
+        if (uploadcare) {
+            this.uploadOnDistant(uri, function (distantUrl) {
+                callback(distantUrl);
             });
         }
-        if (distant) {
-            uploadOnDistant(uri, function () {
-                callback(distantUrl);
+        else {
+            this.downloadLocaly(uri, filename, function (state) {
+                callback(state);
             });
         }
     }
@@ -24,6 +29,9 @@ class Filestorage {
         request.head(uri, function(err, res, body){
             // console.log('content-type:', res.headers['content-type']);
             // console.log('content-length:', res.headers['content-length']);
+            if (!fs.existsSync('./public/posters/')){
+                fs.mkdirSync('./public/posters/');
+            }
             request(uri).pipe(fs.createWriteStream(filename)).on('close', function(){
                 callback(true);
             });
@@ -32,7 +40,7 @@ class Filestorage {
 
     uploadOnDistant (url, callback) {
         //Upload from URL
-        uploadcare.file.fromUrl(url, callback, function(err,res){
+        uploadcare.file.fromUrl(url, function(err,res){
             //Res should contain returned file ID
             let uplc_filepath = STORE_UPLOADCARE_BASE_URL + res.file_id + '/' + res.filename;
             callback(uplc_filepath);
