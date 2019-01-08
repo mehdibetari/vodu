c375da4a646748723f642bec91d0cae5d3586bc3
Betari Mehdi <mehdi.betari@gmail.com>
2019-01-08 16:00:19 +0100

    feat(seo): Add meta description and keywords && fix title

--- src/server/config-server.js
+++ src/server/config-server.js
@@ -2,10 +2,12 @@ const configServer = {
     ALLOSERIE_HOME_LAYOUT: './front-layout/home.html',
     ALLOSERIE_NETFLIX_WEEKLY_URL: 'http://alloserie.fr/episode-chaque-semaine/netflix/',
     ALLOSERIE_NETFLIX_WEEKLY_TITLE: 'Nouveaux épisodes chaque semaines sur Netflix',
+    ALLOSERIE_NETFLIX_WEEKLY_DESCRIPTION: 'Agenda des episodes des series difusé chaque semane sur netflix 2019',
     ALLOSERIE_NETFLIX_WEEKLY_LAYOUT: './front-layout/every-week-netflix.html',
     ALLOSERIE_NETFLIX_WEEKLY_STORE: './store/netflix-every-week.json',
     ALLOSERIE_NETFLIX_CALENDAR_URL: 'http://alloserie.fr/calendrier/netflix/',
-    ALLOSERIE_NETFLIX_CALENDAR_TITLE: 'Agenda Calendrier Netflix 2017 2018 2019',
+    ALLOSERIE_NETFLIX_CALENDAR_TITLE: 'Calendrier Netflix 2019, Agenda des sorties series et films',
+    ALLOSERIE_NETFLIX_CALENDAR_DESCRIPTION: 'Calendrier à jour netflix 2019 et 2020. Contenant l\'agenda de toutes les sorties à venir des films et series ORIGINAL NETFLIX en SVoD pour 2019, 2020 et plus',
     ALLOSERIE_NETFLIX_CALENDAR_LAYOUT: './front-layout/agenda-netflix.html',
     ALLOSERIE_NETFLIX_UPCOMING_STORE: './store/netflix-upcoming.json',
 };
--- src/server/meta.service.js
+++ src/server/meta.service.js
@@ -1,5 +1,5 @@
 
-let configServer = require('./config-server');
+const configServer = require('./config-server').configServer;
 
 function getMediaMetaData (mediaId, items, lastUpdateDate, baseUrl) {
     const media = items.filter(function (item) {
@@ -8,24 +8,30 @@ function getMediaMetaData (mediaId, items, lastUpdateDate, baseUrl) {
     if (media[0] && media[0].premiereDate) {
         return {
             url: baseUrl + mediaId,
-            title: media[0].name + ' - date de sortie: ' + media[0].premiereDate + ' sur Netflix',
-            description: (media[0].summary) ? media[0].summary : 'Calendrier mis à jour le ' + lastUpdateDate,
+            title: `${media[0].name} - date de sortie: ${media[0].premiereDate}  sur Netflix`,
+            description: (media[0].summary) ? `${media[0].name} - ${media[0].summary}` : '',
+            calendarMaj: `Calendrier mis à jour le ${lastUpdateDate}`,
             image: media[0].localPath
         };
     }
     else if (media[0] && media[0].recurence) {
         return {
             url: baseUrl + mediaId,
-            title: media[0].name + ' ' + media[0].recurence + ' sur Netflix',
-            description: (media[0].summary) ? media[0].summary : 'Calendrier mis à jour le ' + lastUpdateDate,
+            title: `${media[0].name} ${media[0].recurence} sur Netflix`,
+            description: (media[0].summary) ? `${media[0].name} - ${media[0].summary}` : '',
+            calendarMaj: `Calendrier mis à jour le ${lastUpdateDate}`,
             image: media[0].localPath
         };
     }
     else {
+        const weekly = baseUrl === configServer.ALLOSERIE_NETFLIX_WEEKLY_URL;
+        const title = weekly ? configServer.ALLOSERIE_NETFLIX_WEEKLY_TITLE : configServer.ALLOSERIE_NETFLIX_CALENDAR_TITLE;
+        const description = weekly ? configServer.ALLOSERIE_NETFLIX_WEEKLY_DESCRIPTION : configServer.ALLOSERIE_NETFLIX_CALENDAR_DESCRIPTION;
         return {
             url: baseUrl,
-            title: (baseUrl === configServer.ALLOSERIE_NETFLIX_WEEKLY_URL) ? configServer.ALLOSERIE_NETFLIX_WEEKLY_TITLE:configServer.ALLOSERIE_NETFLIX_CALENDAR_TITLE,
-            description: 'Calendrier mis à jour le ' + lastUpdateDate,
+            title,
+            description,
+            calendarMaj: `Calendrier mis à jour le ${lastUpdateDate}`,
             image: 'https://ucarecdn.com/342ba0b7-5883-408c-8b19-de42c55a6fa7/alloserielogo2x.jpg'
         };
     }
