d1e75c5cd5de51807db61011a532099fe8a7effb
Betari Mehdi <mehdi.betari@gmail.com>
2019-10-07 16:27:03 +0200

    up form server

--- src/server/router.js
+++ src/server/router.js
@@ -6,7 +6,7 @@ let routesControllers = new RoutesControllers();
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://ergots.fr');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET');
---
+++ vodt-cli.js
@@ -0,0 +1,3 @@
+const tops = require('./src/vodt-run');
+
+tops();
